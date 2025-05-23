0ae9ecd0f3dd4027b15e9074f677910f26921d2a
Gosha Tcherednitchenko <git@gosha.net>
2025-05-23 06:40:10 +0100

    Compiles witout warnings, but at what cost

--- src/SchemeTypes.hs
+++ src/SchemeTypes.hs
@@ -1,22 +1,30 @@
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module SchemeTypes where
+module SchemeTypes (LispVal(..), showVal) where
 
 import Data.Text (Text)
-import qualified Data.Text as T
+import Data.Text qualified as T
 
 -- | Core data type representing any Scheme value
 -- Each constructor represents a different type of value in Ploy
 data LispVal
-  = Atom Text                    -- ^ A symbol, like 'foo or 'bar
-  | List [LispVal]               -- ^ A proper list: (1 2 3)
-  | DottedList [LispVal] LispVal -- ^ An improper list: (1 2 . 3)
-  | Number Integer               -- ^ A whole number
-  -- TODO: Float support
-  | String Text                  -- ^ A string literal
-  | Bool Bool                    -- ^ #t or #f
-  | Nil                          -- ^ The empty list '()
-  deriving stock (Eq)            -- We can compare LispVals for equality
+  = -- | A symbol, like 'foo or 'bar
+    Atom Text
+  | -- | A proper list: (1 2 3)
+    List [LispVal]
+  | -- | An improper list: (1 2 . 3)
+    DottedList [LispVal] LispVal
+  | -- | A whole number
+    -- TODO: Float support
+    Number Integer
+  | -- | A string literal
+    String Text
+  | -- | #t or #f
+    Bool Bool
+  | -- | The empty list '()
+    Nil
+  deriving stock (Eq) -- We can compare LispVals for equality?
 
 -- | Convert a LispVal to its textual representation
 -- This is how values are displayed in the REPL
@@ -28,8 +36,8 @@ showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
 showVal Nil = "()"
 showVal (List vals) = "(" <> T.unwords (map showVal vals) <> ")"
-showVal (DottedList heads tail) =
-  "(" <> T.unwords (map showVal heads) <> " . " <> showVal tail <> ")"
+showVal (DottedList heads tails) =
+  "(" <> T.unwords (map showVal heads) <> " . " <> showVal tails <> ")"
 
 -- Make LispVal an instance of Show for debugging
 instance Show LispVal where
