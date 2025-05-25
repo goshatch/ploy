0d20a32fc993ff7de01bbfffa006662308c074ee
Gosha Tcherednitchenko <git@gosha.net>
2025-05-25 07:07:12 +0100

    Add environment

---
+++ src/Environment.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Environment
+  ( Env,
+    EnvRef,
+    emptyEnv,
+    makeEnvRef,
+    getVar,
+    setVar,
+    defineVar,
+    bindVars,
+  )
+where
+
+import Control.Monad.IO.Class
+import Data.IORef
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
+import Data.Text (Text)
+import SchemeError
+import SchemeTypes
+
+-- | An environment maps variable names to values
+type Env = Map Text LispVal
+
+-- | A mutable reference to an environment
+-- IORef is kind of like an atom in Clojure
+-- https://hackage.haskell.org/package/base-4.21.0.0/docs/Data-IORef.html
+newtype EnvRef = EnvRef (IORef Env)
+
+-- | Create an empty environment
+emptyEnv :: Env
+emptyEnv = Map.empty
+
+-- | Create a mutable environment reference
+makeEnvRef :: Env -> IO EnvRef
+makeEnvRef env = EnvRef <$> newIORef env
+
+-- | Look up a variable
+getVar :: (MonadIO m) => EnvRef -> Text -> m (ThrowsError LispVal)
+getVar (EnvRef ref) var = do
+  env <- liftIO $ readIORef ref
+  return $ case Map.lookup var env of
+    Nothing -> Left $ UnboundVar "Getting an unbound variable" var
+    Just val -> Right val
+
+-- | Set an existing variable
+setVar :: (MonadIO m) => EnvRef -> Text -> LispVal -> m (ThrowsError LispVal)
+setVar envRef@(EnvRef ref) var value = do
+  varExists <- liftIO $ Map.member var <$> readIORef ref
+  if varExists
+    then liftIO $ do
+      modifyIORef ref (Map.insert var value)
+      return $ Right value
+    else return $ Left $ UnboundVar "Setting an unbound variable" var
+
+-- | Define a new variable, or update an existing var
+defineVar :: (MonadIO m) => EnvRef -> Text -> LispVal -> m LispVal
+defineVar (EnvRef ref) var value = liftIO $ do
+  modifyIORef ref (Map.insert var value)
+  return value
+
+-- | Bind multiple vars at once
+bindVars :: EnvRef -> [(Text, LispVal)] -> IO EnvRef
+bindVars (EnvRef ref) bindings = do
+  env <- readIORef ref
+  newRef <- newIORef $ Map.union (Map.fromList bindings) env
+  return $ EnvRef newRef
