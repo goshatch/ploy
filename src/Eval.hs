e8e9d6e311743cba99c4ef5dc20f4ce0a2f01e16
Gosha Tcherednitchenko <git@gosha.net>
2025-05-29 10:40:14 +0100

    Add string? primitive, clean stuff up

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,12 +1,13 @@
 module Eval (eval, primitiveEnv) where
 
+import Control.Monad (when)
 import Control.Monad.Except
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Map.Strict qualified as Map
 import Data.Text (Text)
 import Environment
 import SchemeError
-import Types (EnvRef, IOThrowsError, LispVal (..), showVal)
+import Types (EnvRef, LispVal (..), showVal)
 
 eval :: EnvRef -> LispVal -> IOThrowsError LispVal
 eval envRef = \case
@@ -15,7 +16,6 @@ eval envRef = \case
   val@(Number _) -> return val
   val@(Bool _) -> return val
   Nil -> return Nil
-
   -- Variable reference
   Atom var -> do
     result <- getVar envRef var
@@ -24,12 +24,13 @@ eval envRef = \case
   -- Special forms
   List [Atom "quote", val] ->
     return val
-
-  List [Atom "if", pred, conseq, alt] -> do
-    result <- eval envRef pred
+  List [Atom "if", predicate, conseq, alt] -> do
+    result <- eval envRef predicate
     case result of
       Bool False -> eval envRef alt
-      _ -> eval envRef conseq
+      -- we are pattern-matching on `_other` here because in Scheme, anything
+      -- that is not explicityly #f is considered truthy.
+      _other -> eval envRef conseq
 
   -- TODO: Various other Atoms
 
@@ -47,7 +48,9 @@ apply :: LispVal -> [LispVal] -> IOThrowsError LispVal
 apply = \case
   PrimitiveFunc f -> f -- Primitives handle their own args
   Function params body closure -> \args -> do
-    throwError $ NumArgs (fromIntegral $ length params) args
+    when (length params /= length args) $
+      throwError $
+        NumArgs (fromIntegral $ length params) args
 
     -- Create new environment with parameters bound to arguments
     newEnv <- liftIO $ bindVars closure (zip params args)
@@ -61,12 +64,19 @@ apply = \case
 primitives :: [(Text, [LispVal] -> IOThrowsError LispVal)]
 primitives =
   [ -- Type predicates
-    ("number?", isNumber)
+    ("number?", isNumber),
+    ("string?", isString)
   ]
 
 isNumber :: [LispVal] -> IOThrowsError LispVal
 isNumber = \case
   [Number _] -> return $ Bool True
+  [_] -> return $ Bool False
+  args -> throwError $ NumArgs 1 args
+
+isString :: [LispVal] -> IOThrowsError LispVal
+isString = \case
+  [String _] -> return $ Bool True
   [_] -> return $ Bool False
   args -> throwError $ NumArgs 1 args
 
